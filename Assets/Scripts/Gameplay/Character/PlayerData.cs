abd1916cae791e295b4e133686e541586bb2b6ee
Ajay <ajay.30904@gmail.com>
2023-09-23 01:25:15 -0700

    added dummy floor from sourabh, started working on towerManagement

---
+++ Assets/Scripts/Gameplay/Character/PlayerData.cs
@@ -0,0 +1,30 @@
+// Developed by Pluto
+//
+// Copyright(c) Sora Arts 2023-2024
+//
+// This script is covered by a Non-Disclosure Agreement (NDA) and is Confidential.
+// Destroy the file immediately if you have not been explicitly granted access.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//[Header("")]
+//[Space(0.5f)]
+//[HideInInspector]
+namespace Sora
+{
+    [CreateAssetMenu(menuName = "PlayerControllerData")]
+    public class PlayerData : ScriptableObject
+    {
+        [Header("Movement")]
+        public float moveSpeed;
+        public float meeleRate;
+
+        private void OnValidate()
+        {
+
+        }
+    }
+}
