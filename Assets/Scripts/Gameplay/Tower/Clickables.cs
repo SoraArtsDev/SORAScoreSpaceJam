e777b0254425bb23f7056815f504f8e3b3f3a142
Ajay <ajay.30904@gmail.com>
2023-09-24 02:38:28 -0700

    working on update logic

--- Assets/Scripts/Gameplay/Tower/Clickables.cs
+++ Assets/Scripts/Gameplay/Tower/Clickables.cs
@@ -4,27 +4,41 @@
 //
 // This script is covered by a Non-Disclosure Agreement (NDA) and is Confidential.
 // Destroy the file immediately if you have not been explicitly granted access.
+
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 namespace Sora
 {
-
-    public class Clickable : MonoBehaviour, IPointerClickHandler
+    public class Clickables : MonoBehaviour
     {
-        void OnEnable()
-        {
+        private UnityEvent clickEvent;
 
+        private void Start()
+        {
+            clickEvent = new UnityEvent();
+            clickEvent.AddListener(OnClick);
         }
 
-        public void OnPointerClick(PointerEventData eventData)
+        private void Update()
         {
-            //if (handler.selectedItem != null && eventData.button == PointerEventData.InputButton.Left && eventData.pointerClick.tag == "selectedElement")
-            //{
-            //    handler.DropSelectedItem(transform.gameObject);
-            //}
-            Debug.Log(eventData.pointerClick.tag);
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit) && hit.collider.tag == "Towers")
+                {
+                    clickEvent.Invoke();
+                }
+            }
+        }
+
+        void OnClick()
+        {
+            Debug.Log(gameObject.name);
         }
     }
 }
