9e4fc4effadc6b73f8b367845c4017469222be76
Omkar Patil <theomkarpatil@gmail.com>
2023-09-25 21:32:41 +0530

    Updated Main Menu and scene UI

---
+++ Assets/Scripts/Gameplay/Character/PlayerAnimatorController.cs
@@ -0,0 +1,27 @@
+// Developed by Sora
+//
+// Copyright(c) Sora Arts 2023-2024
+//
+// This script is covered by a Non-Disclosure Agreement (NDA) and is Confidential.
+// Destroy the file immediately if you have not been explicitly granted access.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Sora 
+{
+    /// You may delete all of the stuff inside here. 
+    /// Just remember to stick to the formating
+    public class PlayerAnimatorController : MonoBehaviour
+    {
+        public Animator anim;
+        public NavMeshAgent agent;
+
+        private void Update()
+        {
+            anim.SetFloat("speed", agent.velocity.magnitude);
+        }
+    }
+}
